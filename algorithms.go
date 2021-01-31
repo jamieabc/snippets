388b6bc662dcffc7eb60bb885ca489b973b0b93a
Aaron Chang <xesuschang@gmail.com>
2021-01-31 13:04:30 +0800

    add palindrome function

--- algorithms.go
+++ algorithms.go
@@ -40,3 +40,14 @@ func abs(i int) int {
 	}
 	return -i
 }
+
+// palindrome
+func isPalindrome(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+
+	return true
+}
